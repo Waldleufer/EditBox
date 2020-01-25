b5249ce2de6458f60b4efd1da7c9079b14be69e9
Martin <martin.wagner@fau.de>
2020-01-25 11:53:01 +0100

    Documentation

--- pm.eclipse.editbox/src/pm/eclipse/editbox/impl/ReqIFFileInteraction.java
+++ pm.eclipse.editbox/src/pm/eclipse/editbox/impl/ReqIFFileInteraction.java
@@ -19,22 +19,37 @@ public class ReqIFFileInteraction {
 	public static final int NOT_SIMILAR = -1;
 	public static final int SIMILAR = 0;
 	public static final int EQUAL = 1;
+	
+	// The name of the file located in the Projects "reqif" folder. It can be
+	// altered here if needed.
+	// Further Extension of this tool might include a initial welcome dialog where
+	// the user can set the path to the ReqIF file.
+	private static final String REQIF_FILENAME = "TRCOverlay.reqif";
 
+	// The name aka. the String has to match the Label of the Field of the ReqIF
+	// File that has to be scanned.
+	// So please Specify the Filed in what the unique ID of the Requirement is
+	// stored and the Field whose PlainText shall be displayed.
+	private static final String ID_FIELD_NAME = "requirementID";
+	private static final String TEXTUAL_DESCRIPTION_FIELD_NAME = "PlainText";
 	
 	/**
-	 * TODO: The first run must to set all requirements anew. Otherwise changes in the ReqIF File 
-	 * will never be acknowledged.
+	 * TODO: Changes in the ReqIF File will never be acknowledged. Retrieves the
+	 * information for the Requirements from the ReqIF File as specified in this
+	 * class.
 	 * 
 	 * @param trcList the List of the Requirements whose Info needs to be set.
 	 */
 	public static void setInfos(LinkedList<TRCRequirement> trcList) {
 
-		//TODO: Make it first time Refresh all Reqs
-		
+		// TODO: Change Indication. The Requirements are only read once this way, and
+		// for further clarification after a change to the ReqIF File, Requirements
+		// should be market as invalid
+
 		ReqIF10Parser parser = new ReqIF10Parser();
 		parser.setRemoveTemporaries(true);
 		parser.setRemoveToolExtensions(true);
-		parser.setReqIFFilename("TRCOverlay.reqif");
+		parser.setReqIFFilename(REQIF_FILENAME);
 		ReqIF r = parser.parseReqIFContent();
 		if(r == null) {
 			return;
@@ -53,10 +68,9 @@ public class ReqIFFileInteraction {
 					if(goal == null) {
 						System.out.println("not found");
 					} else {
-						final String DESC = "PlainText";
 
 						SpecElementWithAttributes specWithAttributes = goal.getObject();
-						AttributeValue atNoCast = ReqIF10Util.getAttributeValueForLabel(specWithAttributes, DESC);
+						AttributeValue atNoCast = ReqIF10Util.getAttributeValueForLabel(specWithAttributes, TEXTUAL_DESCRIPTION_FIELD_NAME);
 						AttributeValueStringImpl at = (AttributeValueStringImpl) atNoCast;
 						String desc =  at.getTheValue();
 
@@ -106,10 +120,8 @@ public class ReqIFFileInteraction {
 	 * 
 	 */
 	private static int isSimilar(final String id, final SpecHierarchy spec) {
-		final String ID_LABEL = "requirementID";
-
 		SpecElementWithAttributes specWithAttributes = spec.getObject();
-		AttributeValue atNoCast = ReqIF10Util.getAttributeValueForLabel(specWithAttributes, ID_LABEL);
+		AttributeValue atNoCast = ReqIF10Util.getAttributeValueForLabel(specWithAttributes, ID_FIELD_NAME);
 		// TODO: find a more common way to retreive the ID. Experience told me "8" is the field of the requirement ID;
 		AttributeValueStringImpl at = (AttributeValueStringImpl) atNoCast;
 		String specID =  at.getTheValue();
